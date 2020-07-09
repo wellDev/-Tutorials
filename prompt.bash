e3810a5f104aadb3bbbe720229fcd5bcd56d3ff7
Wellington Santos <tom.correa1@hotmail.com>
2020-07-09 17:30:09 -0300

    Create prompt.bash

---
+++ prompt.bash
@@ -0,0 +1,11 @@
+// Delete files
+rm file_name.ext
+
+// Create a new directory
+mkdir directory_name
+
+// Open a directory
+cd directory_name
+
+// Show all the directories and files into a directory
+ls
